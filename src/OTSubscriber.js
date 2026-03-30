3e3e5becf8ece6713b9bc70153b20b67a4303945
Sukhmanjit Singh <sukhmanjit.singh@caavo.com>
2026-03-30 16:29:04 +0530

    fix: wip

--- src/OTSubscriber.js
+++ src/OTSubscriber.js
@@ -24,6 +24,7 @@ export default class OTSubscriber extends Component {
 
   constructor(props, context) {
     super(props, context);
+    console.log('lol subscriber constructor');
     let initialStreams = getStreams(this.context.sessionId);
     let initialPublisherStream = getPublisherStream(this.context.sessionId);
     if (props.subscribeToSelf && initialPublisherStream) {
